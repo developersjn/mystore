9c79e92281ef2e8e08d179343c3dc98bf9920d01
developersjn <wsammueljn@hotmail.com>
2023-02-27 22:48:36 -0500

    se agrego productos6

--- View/Producto/FindAll.php
+++ View/Producto/FindAll.php
@@ -1,7 +1,7 @@
 <div class="cycle_section layout_padding">
     <div class="container">
         <h1 class="cycle_taital">Modificado desde FindAll</h1>
-        <p class="cycle_text">It is a long established fact that a reader will be distracted by the </p>
+        <p class="cycle_text">subtitulos </p>
         <div class="cycle_section_2 layout_padding">
             <div class="row">
                 <div class="col-md-6">
@@ -11,11 +11,11 @@
                     </div>
                 </div>
                 <div class="col-md-6">
-                    <h1 class="cycles_text">Cycles</h1>
-                    <p class="lorem_text">It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters</p>
+                    <h1 class="cycles_text">Titulo</h1>
+                    <p class="lorem_text">Descripcion</p>
                     <div class="btn_main">
-                        <div class="buy_bt"><a href="#">Buy Now</a></div>
-                        <h4 class="price_text">Price <span style=" color: #f7c17b">$</span> <span style=" color: #325662">200</span></h4>
+                        <div class="buy_bt"><a href="#">Comprar</a></div>
+                        <h4 class="price_text">Precio <span style=" color: #f7c17b">S/</span> <span style=" color: #325662">200</span></h4>
                     </div>
                 </div>
             </div>
